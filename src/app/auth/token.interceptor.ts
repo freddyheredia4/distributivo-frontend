3f4fc65c7ab129fb4d8c232abf7dd63c73ee2163
Max <maxitse7@gmail.com>
2022-08-24 17:48:13 -0500

    screens

--- src/app/auth/token.interceptor.ts
+++ src/app/auth/token.interceptor.ts
@@ -10,20 +10,17 @@ import { Observable } from 'rxjs';
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
 
-  constructor() {}
+  constructor() { }
 
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     console.log(request.url);
-    
-    if (sessionStorage.getItem("seguridad_token")!=null && request.url!="http://localhost:8080/login"){
-      let tokenRequest = request.clone(
-        {
-          headers: request.headers.set('Authorization', String(sessionStorage.getItem('seguridad_token')))
-        }
-      );
-      return next.handle(tokenRequest);
-    }
-    return next.handle(request);
+    let tokenRequest = request.clone(
+      {
+        headers: request.headers.set('Authorization', 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImV4cCI6MTY2MTM2ODY5Niwicm9sIjoiVVNVQVJJT19FTElNSU5BUiJ9.KDml1r8w8DueJSrS9PIkMIAO9TcSstrorCQa-ZArfKPGvypw4Ng3fhJw2gBJRRrqQA08EgWc0v8PC98OoE0M3g')
+      }
+    )
+
+    return next.handle(tokenRequest);
   }
-  
+
 }
---
+++ src/app/feature/teacher/teacher-list/teacher-list.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-teacher-list',
+  templateUrl: './teacher-list.component.html'
+})
+export class TeacherListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
